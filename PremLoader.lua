6670ee9920583619ba8001e29f7fad837d21a88f
haikerkanda <80300203+myladoisbig@users.noreply.github.com>
2026-05-10 21:39:03 +0545

    Update PremLoader.lua

--- PremLoader.lua
+++ PremLoader.lua
@@ -1,8 +1,8 @@
-local gameId = game.GameId
-if gameId == 3823781113 or gameId == 71460947057345 or gameId == 96522925012827 then
+local placeId = game.PlaceId
+if placeId == 3823781113 or placeId == 71460947057345 or placeId == 96522925012827 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/myladoisbig/muladoisreallybig/refs/heads/main/ladomujiautosavegarxa", true))()
-elseif gameId == 99241284776589 then
+elseif placeId == 99241284776589 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DEVIL-Script/DEVIL-Hub/refs/heads/main/OG-Saber-Simulator", true))()
 else
-    print("Unknown Game: " .. gameId)
+    print("Unknown Game: " .. placeId)
 end
